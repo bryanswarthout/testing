471b2ce9116ecd8eb6ade5e7b6ca39b4c9d333be
bryanswarthout <bryanswarthout@github.com>
2020-06-03 10:22:50 -0700

    broken linter

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Requiring necessary npm packages
-var express = require("express");
+var express =  require("express")
 var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
@@ -19,7 +19,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-require("./routes/html-routes.js")(app)
+require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
